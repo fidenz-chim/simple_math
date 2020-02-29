3a8d9cb6caafd39871057c832d45592d23f11a1c
Chim Himidumage <chim@fidenz.com>
2020-02-29 06:36:24 +0530

    fixed merge issues

--- sums.js
+++ sums.js
@@ -1,29 +1,20 @@
 var readlineSync = require('readline-sync');
 
-<<<<<<< HEAD
+
 const NUM_1 = 5;
 const NUM_2 = 5;
 const SHOW_QUESTION_INFO = true;
 const TOTAL_QUESTIONS = 15;
-=======
-const NUM_1 = 3;
-const NUM_2 = 3;
-const SHOW_QUESTION_INFO = true;
-const TOTAL_QUESTIONS = 3;
->>>>>>> 4291275a7490a5ab882373fba470f9dcde9f09e0
 
 function candidateNumber(below){
     return Math.floor(Math.random() * below) + 1
 }
 
-<<<<<<< HEAD
 console.log("");
 console.log("");
 console.log("");
 console.log("");
 
-=======
->>>>>>> 4291275a7490a5ab882373fba470f9dcde9f09e0
 var sessionStart = new Date().getTime() / 1000;
 for (var i=0;i<TOTAL_QUESTIONS;i++) {
 
@@ -33,7 +24,6 @@ for (var i=0;i<TOTAL_QUESTIONS;i++) {
     var problemStart = new Date().getTime() / 1000;
 
     do {
-<<<<<<< HEAD
         var problem = "\t\t\t\t(" + (i+1).toString() + ") " +  num1 + " + " + num2 + " = ";
         var answer = readlineSync.question(problem);
         if (parseInt(answer) === expectedAnswer){
@@ -43,14 +33,6 @@ for (var i=0;i<TOTAL_QUESTIONS;i++) {
 				var dur =  problemEnd - problemStart;
                 console.log("\t\t\t\t\t\tCorrect :) [" + dur.toFixed(2).toString() +" Seconds]");
 			}
-=======
-        var problem = "(" + i.toString() + ") " +  num1 + " + " + num2 + " = ";
-        var answer = readlineSync.question(problem);
-        if (parseInt(answer) === expectedAnswer){
-            var problemEnd = new Date().getTime() / 1000;
-            if (SHOW_QUESTION_INFO)
-                console.log("Correct :) [" + Math.floor(problemEnd - problemStart).toString() +" Seconds]");
->>>>>>> 4291275a7490a5ab882373fba470f9dcde9f09e0
             break;
         }
         else {
